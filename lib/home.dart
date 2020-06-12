d47f7a400b3c76186a6cfcb8639e009f1602e512
Oluwaferanmi-Michael <54253877+Oluwaferanmi-Michael@users.noreply.github.com>
2020-06-12 02:38:56 +0100

    Add files via upload

--- lib/home.dart
+++ lib/home.dart
@@ -1,125 +1,109 @@
+// import 'dart:html';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
-import 'package:quizappteamgold/quizpage.dart';
+import 'package:new_project/quiz.dart';
 
-class homepage extends StatefulWidget {
-
+class homePage extends StatefulWidget {
   @override
-  _homepageState createState() => _homepageState();
+  homePageState createState() => homePageState();
 }
 
-class _homepageState extends State<homepage> {
+class homePageState extends State<homePage> {
 
-  List<String> images =  [
-    "images/py.png",
-    "images/java.png",
-    "images/js.png",
-    "images/cpp.png",
+  List<String> images = [
+    "images/java.png"
+    "images/py.png"
+    "images/Html.png"
+    "images/js.png"
+    "images/cpp.png"
   ];
 
 
-  List<String> des = [
-    "Python is one of the most popular and fastest programming language since half a decade.\nIf You think you have learnt it.. \nJust test yourself !!",
-    "Java has always been one of the best choices for Enterprise World. If you think you have learn the Language...\nJust Test Yourself !!",
-    "Javascript is one of the most Popular programming language supporting the Web.\nIt has a wide range of Libraries making it Very Powerful !",
-    "C++, being a statically typed programming language is very powerful and Fast.\nit's DMA feature makes it more useful. !",
-  ];
-
-
-  Widget customcard(String langname, String image, String desc){
+  Widget customcard(String langname, String image){
     return Padding(
       padding: EdgeInsets.all(
-
-
-        20.0,
+        20.0, 
       ),
       child: InkWell(
         onTap: (){
           Navigator.of(context).pushReplacement(MaterialPageRoute(
-            builder: (context) => getjson(langname),
-          ));
+            builder: (context) => usejson(langname),
+            ));
         },
         child: Material(
-          color: Colors.indigoAccent,
           elevation: 10.0,
-          borderRadius: BorderRadius.circular(20.0),
+          borderRadius: BorderRadius.circular(25.0),
           child: Container(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                  ),
-                  child: Material(
-                    elevation: 5.0,
-                    borderRadius: BorderRadius.circular(100.0),
-                    child: Container(
-                      height: 200,
-                      width: 200,
-                      child: ClipOval(
-                        child: Image(
-                          image: AssetImage(
-                          image,
-                          ),
+            child: Column(children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 20.0,
+                ),
+                child: Material(
+                  child: Container(
+                    height: 200.0,
+                    width: 200.0,
+                    child: ClipOval(
+                      child: Image(
+                        image: AssetImage(
+                          image
                         ),
                       ),
                     ),
                   ),
-                ),
-                Center(
-                  child: Text(
-                    langname,
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      color: Colors.white,
-                      fontFamily: "Quando",
-                      fontWeight: FontWeight.w700,
-                    ),
+                ), 
+              ),
+              Center(child: Text(
+                langname,
+                style: TextStyle(
+                  fontFamily: "Nunito",
+                  fontWeight: FontWeight.w600,
+                  color: Colors.blue
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.all(20.0),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
                 child: Text(
-                  desc,
+                  "Test your skills in $langname.",
                   style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.white,
-                    fontFamily: "Alike"
+                    fontSize: 16.0,
+                    fontFamily: "Nunito",
+                    fontWeight: FontWeight.w300,
+                    color: Colors.blueAccent
                   ),
-                  maxLines: 5,
-                  textAlign: TextAlign.justify,
-                ),
-                ),
-              ],
-            ),
+                  maxLines: 1,
+                  textAlign: TextAlign.center,
+                  ),
+              ),
+            ]),
           ),
         ),
       ),
     );
   }
-  @override
+
+  @override 
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitDown, DeviceOrientation.portraitUp
-    ]);
+  //  SystemChrome.setPreferredOrientations([
+  //    DeviceOrientation.portraitDown, DeviceOrientation.portraitUp
+  //  ]);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Quiz App by Team Gold",
-          style: TextStyle(
-            fontFamily: "Quando",
-          ),
-        )
+          'Quizzes', 
+          style: TextStyle(fontFamily: "Nunito", fontWeight: FontWeight.bold),
+        ),
       ),
       body: ListView(
         children: <Widget>[
-          customcard("Python", images[0], des[0]),
-          customcard("Java", images[1], des[1]),
-          customcard("JavaScript", images[2], des[2]),
-          customcard("C++", images[3], des[3]),
+          customcard("Java", images[0]),
+          customcard("Python", images[1]),
+          customcard("HTML", images[2]),
+          customcard("Javasctipt", images[3]),
+          customcard("C++", images[4]),
         ],
       ),
     );
